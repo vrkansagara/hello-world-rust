c93f213a4b3839dcfd6246475a0169131be6c6eb
Vallabh Kansagara <vrkansagara@gmail.com>
2022-12-19 12:11:03 +0530

    initial hello-world

---
+++ src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let author = "Vallabh Kansagara";
+    let email = "vrkansagara@gmail.com";
+    let github = "https://github.com/vrkansagara";
+    println!("Your github homepage is{0}",github);
+    println!("{0} <{1}>", author, email);
+}
